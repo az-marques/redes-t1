16485287aa70ee0f1fa829f69f1df395b0b7a555
az-marques <85958771+az-marques@users.noreply.github.com>
2024-11-09 14:59:24 -0300

    subindo o q eu fiz até agora
    comentários tão uma bagunça, não organizei ainda

---
+++ RepeatedTimer.py
@@ -0,0 +1,32 @@
+#based on code made available by eraoul and MestreLion in https://stackoverflow.com/a/40965385 under CC BY-SA 3.0
+
+import threading 
+import time
+
+class RepeatedTimer(object):
+  def __init__(self, interval, function, *args, **kwargs):
+    self._timer = None
+    self.interval = interval
+    self.function = function
+    self.args = args
+    self.kwargs = kwargs
+    self.is_running = False
+    self.next_call = time.time()
+    #self.start()
+
+  def _run(self):
+    self.is_running = False
+    self.start()
+    self.function(*self.args, **self.kwargs)
+
+  def start(self):
+    if not self.is_running:
+      self.next_call += self.interval
+      self._timer = threading.Timer(self.next_call - time.time(), self._run)
+      self._timer.start()
+      self.is_running = True
+
+  def stop(self):
+    if self.is_running:
+      self._timer.cancel()
+      self.is_running = False
